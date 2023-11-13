fea93df703fc3a95af91c164902c958869c3acb6
Diego Di Stefano <92904868+diegodistefano@users.noreply.github.com>
2023-11-13 19:14:37 -0300

    Subiendo maquetado propio

---
+++ ReTech - Sitio Web/js/navbar.js
@@ -0,0 +1,65 @@
+const header = document.querySelector("header");
+
+header.innerHTML = `
+<div class="header">
+  <img src="../../images-Sprint1/favicon.png" alt="logo1">
+  <nav class="barra">
+    <ul class="navegacion">
+        <li class="enlace" id="perfil">
+            <a href="#perfil" class="padre"><h5>Menu</h5></a>
+            <div class="submenu">
+              <a href="./index.html">Inicio</a>
+              <a href="./eventos.html">Eventos</a>
+              <a href="./tutoriales.html">Tutoriales</a>
+              <a href="./ubicaciones.html">Ubicaciones</a>
+              <a href="./donaciones.html">Donaciones</a>
+              <a href="./nosotros.html">Nosotros</a>
+              <a href="./faq.html">FAQ</a>
+              <a href="./contacto.html">Contacto</a>
+            </div>
+        </li>
+    </ul>
+  </nav>
+</div>
+
+<script src="https://kit.fontawesome.com/ff6581bb19.js" crossorigin="anonymous"></script>
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // <nav>
+  //   <div class="menu">
+  //     <a href="./index.html">Inicio</a>
+  //     <a href="./eventos.html">Eventos</a>
+  //     <a href="./tutoriales.html">Tutoriales</a>
+  //     <a href="./ubicaciones.html">Ubicaciones</a>
+  //     <a href="./donaciones.html">Donaciones</a>
+  //     <a href="./nosotros.html">Nosotros</a>
+  //     <a href="./faq.html">FAQ</a>
+  //     <a href="./contacto.html">Contacto</a>
+  //   </div>
+  //   <div class="menu-responsive">
+  //     <a href="#perfil" class="enlace">Menu</a>
+  //     <div class="submenu">
+  //       <a href="./index.html">Inicio</a>
+  //       <a href="./eventos.html">Eventos</a>
+  //       <a href="./tutoriales.html">Tutoriales</a>
+  //       <a href="./ubicaciones.html">Ubicaciones</a>
+  //       <a href="./donaciones.html">Donaciones</a>
+  //       <a href="./nosotros.html">Nosotros</a>
+  //       <a href="./faq.html">FAQ</a>
+  //       <a href="./contacto.html">Contacto</a>
+  //     </div>
+  //   </div>
+  // </nav>
